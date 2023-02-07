7b1e10d13e41c02af27c70591d219cd7dbc2d064
Juanrappa <Juanrappa0@gmail.com>
2023-02-07 13:39:04 -0300

    first version of pokedex finished
    finaly the structure of the proyect it remained like this.first, i use the app.js file to create the getPokemons and displayPokemonsHtml functions,there are the main functions of the proyect and also creat the observe function to make a infinite scroll, then the proyect have main.js file and that is for the page functions like functions of button or menu ,the last file is auxiliary-functions.js and this file contain all auxioliary-functions that i use .i choose this structure of file because i wanted that my code was more clean

--- src/app.js
+++ src/app.js
@@ -1,4 +1,31 @@
 const URL = "https://pokeapi.co/api/v2/pokemon/";
-fetch(URL).then(function (respuesta) {
-  console.log(respuesta.json().results);
-});
+const $container = document.querySelector("#containercards");
+let pokemons = ``;
+let next = "";
+const getpokemons = async function (result) {
+  let url = result.url;
+  let data = await fetch(url);
+  let jsondata = await data.json();
+  createPokemonsHTML(jsondata);
+  $container.innerHTML = pokemons;
+};
+const displaypokemons = async function (URL) {
+  let data = await fetch(URL);
+  let jsonData = await data.json();
+  let result = await jsonData.results;
+  next = jsonData.next;
+  await result.forEach(getpokemons);
+  setTimeout(function () {
+    let $cards = document.querySelectorAll("#card");
+    let lastCard = $cards[$cards.length - 1];
+    observer.observe(lastCard);
+  }, 1000);
+};
+let observer = new IntersectionObserver(function (inputs) {
+  inputs.forEach(function (input) {
+    if (input.isIntersecting) {
+      displaypokemons(next);
+    }
+  });
+}, {});
+displaypokemons(URL);
---
+++ src/auxiliary-functions.js
@@ -0,0 +1,43 @@
+const createPokemonsHTML = function (response) {
+  pokemons += `
+    <div id="card" class="max-w-xs rounded overflow-hidden shadow-lg">
+    <img class="w-full" src="${getImg(response)}" alt="Sunset in the mountains">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">${getName(response)}</div>
+      <p class="text-gray-700 text-base">
+        <strong class="font-bold text-lg text-black">tipo:</strong>${getTypes(
+          response
+        )}
+      </p>
+      <p class="text-gray-700 text-base">
+        <strong class="font-bold text-lg text-black">habilidades:</strong>${getAbilities(
+          response
+        )} 
+      </p>
+    </div>
+  </div>
+    `;
+};
+
+const getName = function (response) {
+  let name = response.name;
+  return name;
+};
+const getAbilities = function (response) {
+  let abilities = [];
+  response.abilities.forEach(function (ability) {
+    abilities.push(ability.ability.name);
+  });
+  return abilities;
+};
+const getImg = function (response) {
+  let img = response.sprites.front_default;
+  return img;
+};
+const getTypes = function (response) {
+  let types = [];
+  response.types.forEach(function (type) {
+    types.push(type.type.name);
+  });
+  return types;
+};
